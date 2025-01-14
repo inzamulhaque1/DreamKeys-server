02d9b6aaefffa95fecccbb1069d443108f3ff87e
MD INZAMUL HAQUE JOY <inzamulhaque1002@gmail.com>
2025-01-14 20:56:07 +0600

    user saved to database successfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 
@@ -12,6 +12,73 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Server is running...");
 });
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k3e8u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const usersCollection = client.db("DreamKeys").collection("users")
+
+
+    // USERS
+
+    app.get('/users', async (req, res)=> {
+        const result = await usersCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.post('/users', async(req, res)=> {
+        const user = req.body
+  
+        const query = { email: user.email }
+        const existingUser = await usersCollection.findOne(query)
+  
+        if (existingUser) {
+          return res.send({ message: 'user already exist', insertedID: null })
+        }
+  
+        const result = await usersCollection.insertOne(user)
+        res.send(result)
+      })
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 const PORT = process.env.PORT || 5000;
