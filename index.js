f6c4af13f08a84707082984b14940d56d22d2cb7
MD INZAMUL HAQUE JOY <inzamulhaque1002@gmail.com>
2025-01-15 15:50:55 +0600

    user role added in the dasboard based on the server role

--- index.js
+++ index.js
@@ -22,43 +22,49 @@ console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
 
-    const usersCollection = client.db("DreamKeys").collection("users")
-
-
-    // USERS
-
-    app.get('/users', async (req, res)=> {
-        const result = await usersCollection.find().toArray()
-        res.send(result)
-    })
-
-    app.post('/users', async(req, res)=> {
-        const user = req.body
-  
-        const query = { email: user.email }
-        const existingUser = await usersCollection.findOne(query)
-  
-        if (existingUser) {
-          return res.send({ message: 'user already exist', insertedID: null })
-        }
-  
-        const result = await usersCollection.insertOne(user)
-        res.send(result)
-      })
+        const usersCollection = client.db("DreamKeys").collection("users")
 
 
+        // USERS
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            
+            user.role = user.role || "user";
+
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query)
+
+            if (existingUser) {
+                return res.send({ message: 'user already exist', insertedID: null })
+            }
+
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.get('/users/role', async (req, res) => {
+            const email = req.query.email; // Assuming email is sent as a query parameter
+            const user = await usersCollection.findOne({ email });           
+            res.send({ role: user?.role || "user" });
+          });
 
 
 
@@ -69,13 +75,15 @@ async function run() {
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
